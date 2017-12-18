8de39723b07d71e58151b789a7f6a1480625c14d
natakorn <natakorn@wewillapp.com>
2017-12-18 11:21:10 +0700

    fix HTML indent not work on blade file

--- src/extension.ts
+++ src/extension.ts
@@ -29,6 +29,27 @@ export function activate(context: vscode.ExtensionContext) {
   };
   context.subscriptions.push(vscode.languages.registerDocumentHighlightProvider(documentSelector, new DocumentHighlight))
   context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(documentSelector, new DocumentFormat))
+
+  //Set html indent
+  const EMPTY_ELEMENTS: string[] = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'];
+  vscode.languages.setLanguageConfiguration('blade', {
+      indentationRules: {
+          increaseIndentPattern: /<(?!\?|(?:area|base|br|col|frame|hr|html|img|input|link|meta|param)\b|[^>]*\/>)([-_\.A-Za-z0-9]+)(?=\s|>)\b[^>]*>(?!.*<\/\1>)|<!--(?!.*-->)|\{[^}"']*$/,
+          decreaseIndentPattern: /^\s*(<\/(?!html)[-_\.A-Za-z0-9]+\b[^>]*>|-->|\})/
+      },
+      wordPattern: /(-?\d*\.\d\w*)|([^\`\~\!\@\$\^\&\*\(\)\=\+\[\{\]\}\\\|\;\:\'\"\,\.\<\>\/\s]+)/g,
+      onEnterRules: [
+          {
+              beforeText: new RegExp(`<(?!(?:${EMPTY_ELEMENTS.join('|')}))([_:\\w][_:\\w-.\\d]*)([^/>]*(?!/)>)[^<]*$`, 'i'),
+              afterText: /^<\/([_:\w][_:\w-.\d]*)\s*>$/i,
+              action: { indentAction: vscode.IndentAction.IndentOutdent }
+          },
+          {
+              beforeText: new RegExp(`<(?!(?:${EMPTY_ELEMENTS.join('|')}))(\\w[\\w\\d]*)([^/>]*(?!/)>)[^<]*$`, 'i'),
+              action: { indentAction: vscode.IndentAction.Indent }
+          }
+      ],
+  });
 }
 
 export function deactivate() {
