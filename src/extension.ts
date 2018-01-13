2a7552602b39f6a6662dcab26fae0a75cefe3dbc
Winnie Lin <onecentlin@gmail.com>
2018-01-13 18:25:18 +0800

    Add format configuration setting

--- src/extension.ts
+++ src/extension.ts
@@ -9,18 +9,16 @@ class DocumentHighlight implements vscode.DocumentHighlightProvider {
     let doc = lst.TextDocument.create(document.uri.fsPath, 'html', 1, document.getText())
     return (service.findDocumentHighlights(doc, position, service.parseHTMLDocument(doc)) as any)
   }
-
-}
+} // DocumentHighlight
 
 class DocumentFormat implements vscode.DocumentFormattingEditProvider {
   provideDocumentFormattingEdits(document: vscode.TextDocument, options: vscode.FormattingOptions, token: vscode.CancellationToken): vscode.TextEdit[] | Thenable<vscode.TextEdit[]> {
-    let doc = lst.TextDocument.create(document.uri.fsPath, 'html', 1, document.getText())
+    let doc = lst.TextDocument.create(document.uri.fsPath, 'blade', 1, document.getText())
     let range = lst.Range.create(lst.Position.create(0, 0), lst.Position.create(doc.lineCount, 1))
-    let format = (vscode.workspace.getConfiguration('html.format') as any)
+    let format = (vscode.workspace.getConfiguration('blade.format') as any)
     return (service.format(doc, range, format) as any)
   }
-
-}
+} // DocumentFormat
 
 export function activate(context: vscode.ExtensionContext) {
   let documentSelector: vscode.DocumentSelector = {
@@ -28,9 +26,13 @@ export function activate(context: vscode.ExtensionContext) {
     scheme: 'file'
   };
   context.subscriptions.push(vscode.languages.registerDocumentHighlightProvider(documentSelector, new DocumentHighlight))
-  context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(documentSelector, new DocumentFormat))
 
-  //Set html indent
+  let bladeFormat = vscode.workspace.getConfiguration('blade.format');
+  if (bladeFormat.get('enable')) {
+    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(documentSelector, new DocumentFormat))
+  }
+
+  // Set html indent
   const EMPTY_ELEMENTS: string[] = ['area', 'base', 'br', 'col', 'embed', 'hr', 'img', 'input', 'keygen', 'link', 'menuitem', 'meta', 'param', 'source', 'track', 'wbr'];
   vscode.languages.setLanguageConfiguration('blade', {
       indentationRules: {
