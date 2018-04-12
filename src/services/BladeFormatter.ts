c91a47f021108ac388fea6615ce51bf6a313d2ec
Lucas Snel <luacs@compareweb.org>
2018-04-12 12:30:39 +0200

    Fix issue #65: no empty new line after @extends and self-closign @section

--- src/services/BladeFormatter.ts
+++ src/services/BladeFormatter.ts
@@ -1,45 +1,47 @@
-export class BladeFormatter 
-{
-    newLine: string = "\n";
-    indentPattern: string;
+export class BladeFormatter {
+	newLine: string = "\n";
+	indentPattern: string;
 
-    constructor(options?: IBladeFormatterOptions) {
-        options = options || {};
+	constructor(options?: IBladeFormatterOptions) {
+		options = options || {};
 
-        // options default value
-        options.tabSize = options.tabSize || 4;
-        if (typeof options.insertSpaces === "undefined") {
-            options.insertSpaces = true;
-        }
+		// options default value
+		options.tabSize = options.tabSize || 4;
+		if (typeof options.insertSpaces === "undefined") {
+			options.insertSpaces = true;
+		}
 
-        this.indentPattern = (options.insertSpaces) ? " ".repeat(options.tabSize) : "\t";
-    }
+		this.indentPattern = (options.insertSpaces) ? " ".repeat(options.tabSize) : "\t";
+	}
 
-    format(inuptText: string): string {
-        
-        let inComment: boolean = false;
-        let output: string = inuptText;
+	format(inuptText: string): string {
 
-        // block pattern
-        let patternBlock = /(\@)(inject|extends|section|hasSection|include|stop|endpush|endphp)/g;
+		let inComment: boolean = false;
+		let output: string = inuptText;
 
-        // blade format fix
-        output = output.replace(patternBlock, function (match: string) {
-            return "\n" + match;
-        });
+		// block pattern
+		let patternBlock = /(\@)(inject|extends|section|hasSection|include|stop|endpush|endphp)/g;
 
-        output = output.replace(/(\s*)\@include/g, "\n" + this.indentPattern + "@include");
-        output = output.replace(/(\s*)\@endsection/g, "\n@endsection\n");
+		// blade format fix
+		output = output.replace(patternBlock, function (match: string) {
+			return "\n" + match;
+		});
 
-        // fix #57 url extra space after formatting
-        output = output.replace(/url\(\"(\s*)/g, "url\(\"");
+		output = output.replace(/(\s*)\@include/g, "\n" + this.indentPattern + "@include");
+		output = output.replace(/(\s*)\@endsection/g, "\n@endsection\n");
 
-        return output.trim();
-    }
+		output = output.replace(/(\@(section|yield)\(.*\',.*|\@extends\(.*\))/g, function (match: string) {
+			return match + "\n";
+		});
+
+		// fix #57 url extra space after formatting
+		output = output.replace(/url\(\"(\s*)/g, "url\(\"");
+
+		return output.trim();
+	}
 }
 
-export interface IBladeFormatterOptions 
-{
-    insertSpaces?: boolean;
-    tabSize?: number;
+export interface IBladeFormatterOptions {
+	insertSpaces?: boolean;
+	tabSize?: number;
 }
